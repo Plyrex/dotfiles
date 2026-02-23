e95e4d595ca0a14ec45cc06c84edf13accf60228
Saeed Khan <saeedrkhan12@gmail.com>
2026-02-22 21:47:10 -0400

    chore: added fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,29 @@
+# source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+starship init fish | source
+
+# overwrite greeting
+# potentially disabling fastfetch
+function fish_greeting
+    fastfetch
+end
+
+# Alias' taken from CachyOS fish config
+# Replace ls with eza
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
+
+# Common use alias
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
